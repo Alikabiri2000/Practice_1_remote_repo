b1ba27817b41638919ea3a8bae7303d90e0cbb90
Ali_Kabiri_N1 <alikabirimyb@gmail.com>
2026-06-11 08:16:01 +0330

    Insert values into students table

--- c.sql
+++ c.sql
@@ -3,5 +3,19 @@ USE school;
 
 CREATE TABLE students (
 std_id INT,
-name VARCHAR (40),
+full_name VARCHAR (40),
 major VARCHAR (40));
+
+DROP TABLE students;
+
+ALTER TABLE students
+ADD PRIMARY KEY (std_id);
+
+DESCRIBE students;
+
+INSERT INTO students (std_id, full_name, major)
+VALUES (1, 'Ali Kabiri', 'MBA'),
+(2, 'Hossein Sajedi', 'Math'),
+(3, Null, 'Data sci');
+
+SELECT * FROM students;
